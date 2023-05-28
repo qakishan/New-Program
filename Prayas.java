ba468dc61556bbec996abc37c64a372d1c64c5fc
qakishan <121118010+qakishan@users.noreply.github.com>
2023-05-28 17:47:36 +0530

    Add files via upload

---
+++ Prayas.java
@@ -0,0 +1,16 @@
+package pProgram;
+
+public class Prayas {
+	public static void main(String[] args) {
+	char c='%';
+	if(c>='a'&&c<='z')
+	{
+		System.out.println("alphabet");
+	}
+	else
+	{
+		System.out.println("not alphabet");
+	}
+	}
+
+}
